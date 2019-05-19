7a0622edd9aa028147f2b3d254fa0b6e01db0a93
Adam Parslow <adam.parslow@hotmail.com>
2019-05-19 17:41:22 +1000

    Some minor corrections

--- server.js
+++ server.js
@@ -28,7 +28,9 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
-const port = process.env.PORT || 25565;
+const port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+// const ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+// const port = 8080;
 
 app.get("/", (req, res) => {
     fs.readFile("index.html", (err, data) => {
