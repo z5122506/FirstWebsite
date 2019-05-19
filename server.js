ce5d077abfa73355b5ec0f12bb57cdfdacfa96a5
Adam Parslow <z5122506@student.unsw.edu.au>
2019-05-19 17:28:40 +1000

    Basic server

---
+++ server.js
@@ -0,0 +1,43 @@
+// const http = require('http');
+// const fs = require('fs');
+// const process = require('process')
+
+// console.log(process.argv);
+// // let hostname = "127.0.0.1";
+// if (process.argv.length > 2) {
+//     hostname = "127.0.0.1";
+// } else {
+//     // TODO: Set this to dynamic host names
+//     hostname = "10.0.0.11";
+// }
+// const port = 25565;
+
+// const server = http.createServer((req, res) => {
+//     fs.readFile("index.html", (err, data) => {
+//         res.statusCode = 200;
+//         res.setHeader('Content-Type', 'text/html');
+//         res.end(data);
+//     });
+// });
+
+// server.listen(port, hostname, () => {
+//     console.log("Server running at http://" + hostname + ":" + port + "/");
+// });
+
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+const port = process.env.PORT || 25565;
+
+app.get("/", (req, res) => {
+    fs.readFile("index.html", (err, data) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(data);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+})
