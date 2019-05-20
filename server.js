ac0f8166b06e4a115f6267aff1704afd8f80ab52
Adam Parslow <adam.parslow@hotmail.com>
2019-05-20 16:12:35 +1000

    Added react client

--- server.js
+++ server.js
@@ -1,40 +1,14 @@
-// const http = require('http');
-// const fs = require('fs');
-// const process = require('process')
-
-// console.log(process.argv);
-// // let hostname = "127.0.0.1";
-// if (process.argv.length > 2) {
-//     hostname = "127.0.0.1";
-// } else {
-//     // TODO: Set this to dynamic host names
-//     hostname = "10.0.0.11";
-// }
-// const port = 25565;
-
-// const server = http.createServer((req, res) => {
-//     fs.readFile("index.html", (err, data) => {
-//         res.statusCode = 200;
-//         res.setHeader('Content-Type', 'text/html');
-//         res.end(data);
-//     });
-// });
-
-// server.listen(port, hostname, () => {
-//     console.log("Server running at http://" + hostname + ":" + port + "/");
-// });
-
 const express = require('express');
 const fs = require('fs');
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static('client/build'));
 
 const port = process.env.PORT || 8080;
 // const ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 // const port = 8080;
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
     fs.readFile("index.html", (err, data) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html');
